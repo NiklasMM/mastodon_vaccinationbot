33bf9518cc557796011d7d68407985a226a5daf6
Niklas Meinzer <github@niklas-meinzer.de>
2021-03-18 21:30:00 +0100

    feat(toot): Add change percentages

--- main.py
+++ main.py
@@ -19,15 +19,46 @@ def format_number(number):
     return f"{number:,}".replace(",", ".")
 
 
+def format_percentage(number):
+    if number >= 10:
+        emoji = "⬆️"
+    elif number < 10 and number >= 1:
+        emoji = "↗️"
+    elif number < 1 and number > -1:
+        emoji = "➡️"
+    elif number <= -1 and number > -10:
+        emoji = "↘️"
+    else:
+        emoji = "⬇️"
+    return f"{number:.2f}% {emoji})"
+
+
 def generate_toot(container):
     data_yesterday = container[datetime.date.today() - datetime.timedelta(days=1)]
     eight_days_ago = container[datetime.date.today() - datetime.timedelta(days=8)]
 
+    doses_yesterday = data_yesterday.doses_new
+    doses_week_before = eight_days_ago.doses_new
+
+    percentage_total = (doses_yesterday / doses_week_before - 1) * 100
+
+    average_yesterday = container.sevenDayAverage()
+    average_week_before = container.sevenDayAverage(eight_days_ago.date)
+    percentage_average = (average_yesterday / average_week_before - 1) * 100
+
     message = (
-        f"Impfungen gestern ({yesterday.isoformat()}): {format_number(data_yesterday.doses_new)}\n"
-        f"Impfungen eine Woche zuvor: {format_number(eight_days_ago.doses_new)}\n\n"
-        f"7-Tage-Durchschnitt gestern: {format_number(int(container.sevenDayAverage()))}\n"
-        f"7-Tage-Durchschnitt eine Woche zuvor: {format_number(int(container.sevenDayAverage(eight_days_ago.date)))}"
+        "Impfungen gestern ({yesterday_date}): {yesterday_doses} ({percentage_total})\n"
+        "Impfungen eine Woche zuvor: {last_week_doses}\n\n"
+        "7-Tage-Durchschnitt gestern: {average} ({percentage_average})\n"
+        "7-Tage-Durchschnitt eine Woche zuvor: {average_last_week}"
+    ).format(
+        yesterday_date=yesterday.isoformat(),
+        yesterday_doses=format_number(doses_yesterday),
+        last_week_doses=format_number(doses_week_before),
+        average=format_number(int(average_yesterday)),
+        average_last_week=format_number(int(average_week_before)),
+        percentage_total=format_percentage(percentage_total),
+        percentage_average=format_percentage(percentage_average),
     )
     return message
 
